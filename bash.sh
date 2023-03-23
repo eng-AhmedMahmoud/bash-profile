0101ac9a0ff15911fccdef3919cdee032512eacb
Ahmed Mahmoud <ahmeddahabmohamed@gmail.com>
2023-03-23 12:05:14 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -10,13 +10,13 @@ alias rm="git rm"
 alias init="git init"
 alias br="git branch"
 alias clone="git clone"
-alias ph="git push origin master"
+alias ph="git push origin main"
 
 # access user name ,eamil and password
 alias gitconfig='function _gitconfig() { git config --global user.name "$1"; git config --global user.email "$2";}; _gitconfig'
 
 # print user name and email and password
-alias gitconfigprint='function _gitconfigprint() { git config --global user.name; git config --global user.email;}; _gitconfigprint'
+alias config='function _gitconfigprint() { git config --global user.name; git config --global user.email;}; _gitconfigprint'
 
 # add and commit
 alias repo='git init && git add . && git commit -m "Initial commit"'
